29b934067da32519465c6611d079d2b775dac55f
rowanjb <rjb641@mun.ca>
2025-01-06 17:52:19 +0100

    Making T and S bins from climatology

--- make_binary.py
+++ make_binary.py
@@ -8,9 +8,85 @@ import xmitgcm.file_utils
 import xmitgcm.utils
 import xarray as xr
 
+import sys
+sys.path.insert(1, '../obs_analyses/')
+import mooring_analyses
+import woa_analyses
+
+def temp_from_woa():
+    dst,dss = woa_analyses.get_woa('autumn',2015)
+    dy = np.array([
+         0.4, 1.2, 2.0, 2.8, 3.6, 4.4, 5.2, 6.0, 6.8, 7.6,
+         8.4, 9.2,10.0,10.8,11.6,12.4,13.2,14.0,14.8,15.6,
+        16.4,17.2,18.0,18.8,19.6,20.4,21.2,22.0,22.8,23.6,
+        24.4,25.2,26.0,26.8,27.6,28.4,29.2,30.0,30.8,31.6,
+        32.4,33.2,34.0,34.8,35.6,36.4,37.2,38.0,38.8,39.6])
+    y = np.zeros(len(dy))
+    for i,n in enumerate(dy):
+        if i==0: y[i] = n/2
+        else: y[i] = np.sum(dy[:i]) + n/2
+    T = dst['t_an'].isel(time=0).sel({'lat': -69, 'lon': -27},method='nearest').interp(depth=y)
+    S = dss['s_an'].isel(time=0).sel({'lat': -69, 'lon': -27},method='nearest').interp(depth=y)
+    pseudo = np.tile(T,(100,100,1))
+    xmitgcm.utils.write_to_binary(pseudo.flatten(order='F'), 'T.WOA.bin')
+    pseudo = np.tile(S,(100,100,1))
+    xmitgcm.utils.write_to_binary(pseudo.flatten(order='F'), 'S.WOA.bin')
+    
+    #testing
+    T2 = xmitgcm.utils.read_raw_data('T.WOA.bin', shape=(50,100,100), dtype=np.dtype('>f4') )
+    X = np.linspace(0, 99, 100)
+    Y = np.linspace(0, 49, 50)
+    fig, ax = plt.subplots()
+    cs = ax.contourf(X, Y, T2[:,49,:])
+    cbar = fig.colorbar(cs)
+    plt.savefig('T.WOA.png')
+    print(T2[0,:,:])
+    print(T2[:,20,20])
+    print(T2[:,80,80])
+    quit()
+
+
+def temp_from_mooring():
+    ds = mooring_analyses.open_mooring_ml_data()
+    dy = (-1)*np.array([
+         0.4, 1.2, 2.0, 2.8, 3.6, 4.4, 5.2, 6.0, 6.8, 7.6,
+         8.4, 9.2,10.0,10.8,11.6,12.4,13.2,14.0,14.8,15.6,
+        16.4,17.2,18.0,18.8,19.6,20.4,21.2,22.0,22.8,23.6,
+        24.4,25.2,26.0,26.8,27.6,28.4,29.2,30.0,30.8,31.6,
+        32.4,33.2,34.0,34.8,35.6,36.4,37.2,38.0,38.8,39.6])
+    y = np.zeros(len(dy))
+    for i,n in enumerate(dy):
+        if i==0: y[i] = n/2
+        else: y[i] = np.sum(dy[:i]) + n/2
+    ds = ds.assign_coords({'model_depths': y})
+    print(ds['T'].isel(day=250).interp(depth=y).to_numpy())
+    quit()
+
+    lon = np.arange(0,100,1)
+    lat = np.arange(0,100,1)
+    depth = np.arange(0,50,1)
+    shape,_,_ = np.meshgrid(depth,lon,lat,indexing='ij')
+    T = ds['T'].isel(day=250).to_numpy()
+    S = ds['S'].isel(day=250).to_numpy()
+    dy = np.array([
+         0.4, 1.2, 2.0, 2.8, 3.6, 4.4, 5.2, 6.0, 6.8, 7.6,
+         8.4, 9.2,10.0,10.8,11.6,12.4,13.2,14.0,14.8,15.6,
+        16.4,17.2,18.0,18.8,19.6,20.4,21.2,22.0,22.8,23.6,
+        24.4,25.2,26.0,26.8,27.6,28.4,29.2,30.0,30.8,31.6,
+        32.4,33.2,34.0,34.8,35.6,36.4,37.2,38.0,38.8,39.6])
+    y = np.zeros(len(dy))
+    for i,n in enumerate(dy):
+        if i==0: y[i] = n/2
+        else: y[i] = np.sum(dy[:i]) + n/2 
+    df = pd.DataFrame(data={'y': y, 'dy': dy})
+    print(df)
+    quit()
+
+
 def basic_example():
     lon = np.arange(0,100,1)
     lat = np.arange(0,100,1)
+    quit()
     depth = np.arange(0,50,1)
     shape,_,_ = np.meshgrid(depth,lon,lat)
     pseudo = shape*0+1
@@ -80,4 +156,4 @@ def argo_temp_profile():
 #print(ds.PRES_QC.values)
 
 if __name__ == "__main__":
-    basic_example()
+    temp_from_woa()
