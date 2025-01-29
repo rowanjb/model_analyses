20d6e2f5c52655b7abdf62b93ee7d081bac8a6c0
rowanjb <rjb641@mun.ca>
2025-01-29 17:02:42 +0100

    Created 150x150 binaries

--- make_binary.py
+++ make_binary.py
@@ -1,4 +1,5 @@
-# Copied from: https://xmitgcm.readthedocs.io/en/stable/demo_writing_binary_file.html 
+# Based on: https://xmitgcm.readthedocs.io/en/stable/demo_writing_binary_file.html 
+# These are some basic functions that I use to make binaries
 
 import numpy as np
 import pandas as pd
@@ -13,8 +14,11 @@ sys.path.insert(1, '../obs_analyses/')
 import mooring_analyses
 import woa_analyses
 
-def temp_from_woa():
-    dst,dss = woa_analyses.get_woa('autumn',2015)
+def from_woa():
+    dst = woa_analyses.get_woa_Weddell_mooring('autumn',2015,'t')
+    dss = woa_analyses.get_woa_Weddell_mooring('autumn',2015,'s')
+    print(dst)
+    print(dss)
     dy = np.array([
          0.4, 1.2, 2.0, 2.8, 3.6, 4.4, 5.2, 6.0, 6.8, 7.6,
          8.4, 9.2,10.0,10.8,11.6,12.4,13.2,14.0,14.8,15.6,
@@ -25,111 +29,94 @@ def temp_from_woa():
     for i,n in enumerate(dy):
         if i==0: y[i] = n/2
         else: y[i] = np.sum(dy[:i]) + n/2
-    T = dst['t_an'].isel(time=0).sel({'lat': -69, 'lon': -27},method='nearest').interp(depth=y)
-    S = dss['s_an'].isel(time=0).sel({'lat': -69, 'lon': -27},method='nearest').interp(depth=y)
-    pseudo = np.tile(T,(100,100,1))
-    xmitgcm.utils.write_to_binary(pseudo.flatten(order='F'), 'T.WOA.bin')
-    pseudo = np.tile(S,(100,100,1))
-    xmitgcm.utils.write_to_binary(pseudo.flatten(order='F'), 'S.WOA.bin')
+    T = dst.isel(time=0).interp(depth=y)
+    S = dss.isel(time=0).interp(depth=y)
+    pseudo = np.tile(T,(150,150,1))
+    xmitgcm.utils.write_to_binary(pseudo.flatten(order='F'), '../MITgcm/so_plumes/binaries/T.WOA.150x150.bin')
+    pseudo = np.tile(S,(150,150,1))
+    xmitgcm.utils.write_to_binary(pseudo.flatten(order='F'), '../MITgcm/so_plumes/binaries/S.WOA.150x150.bin')
     
     #testing
-    T2 = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/T.WOA.bin', shape=(50,100,100), dtype=np.dtype('>f4') )
-    X = np.linspace(0, 99, 100)
+    T2 = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/T.WOA.150x150.bin', shape=(50,150,150), dtype=np.dtype('>f4') )
+    X = np.linspace(0, 149, 150)
     Y = np.linspace(0, 49, 50)
     fig, ax = plt.subplots()
-    cs = ax.contourf(X, Y, T2[:,49,:])
+    cs = ax.pcolormesh(X, Y, T2[:,49,:])
     cbar = fig.colorbar(cs)
-    plt.savefig('T.WOA.png')
-    print(T2[0,:,:])
-    print(T2[:,20,20])
-    print(T2[:,80,80])
+    plt.savefig('T.WOA.150x150.png')
+
+    S2 = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/S.WOA.150x150.bin', shape=(50,150,150), dtype=np.dtype('>f4') )
+    X = np.linspace(0, 149, 150)
+    Y = np.linspace(0, 49, 50)
+    fig, ax = plt.subplots()
+    cs = ax.pcolormesh(X, Y, S2[:,49,:])
+    cbar = fig.colorbar(cs)
+    plt.savefig('S.WOA.150x150.png')
 
 def new_Q_surf():
     Q = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/Qnet_p32.bin', shape=(100,100), dtype=np.dtype('>f4') ) 
-    newQ = Q/6
-    xmitgcm.utils.write_to_binary(newQ.flatten(order='F'), '../MITgcm/so_plumes/binaries/Qnet_WOA.bin')
+    newQ = np.zeros((150,150))
+    newQ[25:125,25:125] = Q/12
+    xmitgcm.utils.write_to_binary(newQ.flatten(order='F'), '../MITgcm/so_plumes/binaries/Qnet_WOA2.150x150.bin')
 
-    Q = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/Qnet_WOA.bin', shape=(100,100), dtype=np.dtype('>f4') )
-    X = np.linspace(0, 99, 100)
-    Y = np.linspace(0, 99, 100)
+    Q = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/Qnet_WOA2.150x150.bin', shape=(150,150), dtype=np.dtype('>f4') )
+    X = np.linspace(0, 149, 150)
+    Y = np.linspace(0, 149, 150)
     fig, ax = plt.subplots()
-    cs = ax.contourf(X, Y, Q)
+    cs = ax.pcolormesh(X, Y, Q) #contourf
     cbar = fig.colorbar(cs)
-    plt.savefig('Qnet_WOA.png')
+    plt.savefig('Qnet_WOA2.150x150.bin.png')
 
 def new_Eta():
-    Eta = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/Eta.120mn.bin', shape=(100,100), dtype=np.dtype('>f4') )
+    #Eta = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/Eta.120mn.bin', shape=(100,100), dtype=np.dtype('>f4') )
+    Eta = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/Qnet_WOA2.150x150.bin', shape=(150,150), dtype=np.dtype('>f4') )
     Eta[Eta != np.isnan] = 0
-    xmitgcm.utils.write_to_binary(Eta.flatten(order='F'), '../MITgcm/so_plumes/binaries/Eta.flat.bin')
+    xmitgcm.utils.write_to_binary(Eta.flatten(order='F'), '../MITgcm/so_plumes/binaries/Eta.flat.150x150.bin')
 
-    Eta = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/Eta.flat.bin', shape=(100,100), dtype=np.dtype('>f4') )
+    Eta = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/Eta.flat.150x150.bin', shape=(150,150), dtype=np.dtype('>f4') )
     #testing
-    X = np.linspace(0, 99, 100)
-    Y = np.linspace(0, 99, 100)
+    X = np.linspace(0, 149, 150)
+    Y = np.linspace(0, 149, 150)
     fig, ax = plt.subplots()
-    cs = ax.contourf(X, Y, Eta)
+    cs = ax.pcolormesh(X, Y, Eta)
     cbar = fig.colorbar(cs)
-    plt.savefig('Eta.flat.png')
+    plt.savefig('Eta.flat.150x150.png')
 
 def new_U():
-    U = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/U.120mn.bin', shape=(50,100,100), dtype=np.dtype('>f4') )
+    #U = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/U.120mn.bin', shape=(50,100,100), dtype=np.dtype('>f4') )
+    U = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/T.WOA.150x150.bin', shape=(50,150,150), dtype=np.dtype('>f4') )
     U[U != np.isnan] = 0 
-    xmitgcm.utils.write_to_binary(U.flatten(order='F'), '../MITgcm/so_plumes/binaries/U.motionless.bin') #might need to be "C" if not all 0s
+    xmitgcm.utils.write_to_binary(U.flatten(order='F'), '../MITgcm/so_plumes/binaries/U.motionless.150x150.bin') #might need to be "C" if not all 0s
 
-    U = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/U.motionless.bin', shape=(50,100,100), dtype=np.dtype('>f4') )
-    #testing
-    X = np.linspace(0, 99, 100)
+    # testing
+    U = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/U.motionless.150x150.bin', shape=(50,150,150), dtype=np.dtype('>f4') )
+    X = np.linspace(0, 149, 150)
     Y = np.linspace(0, 49, 50)
     fig, ax = plt.subplots()
-    cs = ax.contourf(X, Y, U[:,49,:])
+    cs = ax.pcolormesh(X, Y, U[:,49,:])
     cbar = fig.colorbar(cs)
-    plt.savefig('U.motionless.png')
+    plt.savefig('U.motionless.150x150.png')
     print(U[0,:,:])
     print(U[:,20,20])
     print(U[:,80,80])
 
 def new_V():
-    V = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/V.120mn.bin', shape=(50,100,100), dtype=np.dtype('>f4') )
+    #V = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/V.120mn.bin', shape=(50,100,100), dtype=np.dtype('>f4') )
+    V = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/T.WOA.150x150.bin', shape=(50,150,150), dtype=np.dtype('>f4') )
     V[V != np.isnan] = 0
-    xmitgcm.utils.write_to_binary(V.flatten(order='F'), '../MITgcm/so_plumes/binaries/V.motionless.bin') #might need to be "C" if not all 0s
+    xmitgcm.utils.write_to_binary(V.flatten(order='F'), '../MITgcm/so_plumes/binaries/V.motionless.150x150.bin') #might need to be "C" if not all 0s
 
-    V = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/V.motionless.bin', shape=(50,100,100), dtype=np.dtype('>f4') )
-    #testing
-    X = np.linspace(0, 99, 100)
+    # testing
+    V = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/V.motionless.150x150.bin', shape=(50,150,150), dtype=np.dtype('>f4') )
+    X = np.linspace(0, 149, 150)
     Y = np.linspace(0, 49, 50)
     fig, ax = plt.subplots()
-    cs = ax.contourf(X, Y, V[:,49,:])
+    cs = ax.pcolormesh(X, Y, V[:,49,:])
     cbar = fig.colorbar(cs)
-    plt.savefig('V.motionless.png')
+    plt.savefig('V.motionless.150x150.png')
     print(V[0,:,:])
     print(V[:,20,20])
     print(V[:,80,80])
-
-def relax_mask():
-    # opening any old file simply to get the right array shape
-    M = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/V.120mn.bin', shape=(50,100,100), dtype=np.dtype('>f4') )
-    M[:] = 0 # interior cells should be 0
-    M[:,1,:] = 0.25 # second to outer cells are 0.25---I copied this from the reentrant channel ex., but I assume it's to avoid numerical problems
-    M[:,98,:] = 0.25
-    M[:,:,1] = 0.25
-    M[:,:,98] = 0.25
-    M[48,:,:] = 0.25
-    M[:,0,:] = 1 # outer cells are 1---note they need to be spec'd after the 0.25 cells to avoid make some outter cells 0.25 accidentally
-    M[:,99,:] = 1
-    M[:,:,0] = 1
-    M[:,:,99] = 1
-    M[49,:,:] = 1
-    # either np.moveaxis(M, [0, 1, 2], [-1, -2, -3]).flatten(order='F') or M.flatten(order='C')
-    xmitgcm.utils.write_to_binary(np.moveaxis(M, [0, 1, 2], [-1, -2, -3]).flatten(order='F'), '../MITgcm/so_plumes/binaries/relax_mask_incl_bottom.bin')
-
-    #testing
-    M = xmitgcm.utils.read_raw_data('../MITgcm/so_plumes/binaries/relax_mask_incl_bottom.bin', shape=(50,100,100), dtype=np.dtype('>f4') )
-    X = np.linspace(0, 99, 100)
-    Y = np.linspace(0, 49, 50)
-    fig, ax = plt.subplots()
-    cs = ax.pcolormesh(X, Y, M[:,:,25])
-    cbar = fig.colorbar(cs)
-    plt.savefig('relax_mask.png')
 
 def basic_example():
     lon = np.arange(0,100,1)
@@ -141,9 +128,8 @@ def basic_example():
     xmitgcm.utils.write_to_binary(pseudo.flatten(), 'T.1c_new.bin', dtype=np.dtype('>f4') )
 
 if __name__ == "__main__":
-    #temp_from_woa()
+    #from_woa()
     #new_Q_surf()
     #new_Eta()
     #new_U()
     #new_V()
-    relax_mask()
