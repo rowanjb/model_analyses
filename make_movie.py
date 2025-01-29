b59b4826adfe5befc9d0585463268e0877942dda
rowanjb <rjb641@mun.ca>
2025-01-29 17:03:13 +0100

    Adding end statement

--- make_movie.py
+++ make_movie.py
@@ -9,7 +9,8 @@ def numericalSort(value):
     parts[1::2] = map(int, parts[1::2])
     return parts
 
-def generate_video(fig_dir,run):
+def generate_video(run):
+    fig_dir = './figs_'+run+'/'
     figs = sorted(os.listdir(fig_dir))#,key=numericalSort)
     frame = cv2.imread(fig_dir + figs[0])
     height, width, layers = frame.shape
@@ -23,13 +24,17 @@ def generate_video(fig_dir,run):
         video.write(img)
     cv2.destroyAllWindows()
     video.release()
+    print(run + ' video saved')
 
 if __name__ == "__main__":
-    #generate_video('./figs_mrb_002/','mrb_002')
-    #generate_video('./figs_mrb_003/','mrb_003')
-    #generate_video('./figs_mrb_004/','mrb_004')
-    #generate_video('./figs_mrb_005/','mrb_005')
-    #generate_video('./figs_mrb_006/','mrb_006')
-    #generate_video('./figs_mrb_007/','mrb_007')
-    generate_video('./figs_mrb_008/','mrb_008')
+    generate_video('mrb_002')
+    generate_video('mrb_003')
+    generate_video('mrb_004')
+    generate_video('mrb_005')
+    generate_video('mrb_006')
+    generate_video('mrb_007')
+    generate_video('mrb_008')
+    #generate_video('mrb_009')
+    #generate_video('mrb_010')
+    #generate_video('mrb_011')
     
