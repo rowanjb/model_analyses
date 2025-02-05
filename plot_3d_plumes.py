41d2b27b3fcf41e2c848f93ac63c6695d470b4c4
rowanjb <rjb641@mun.ca>
2025-02-05 18:21:12 +0100

    About to make significant changes

--- plot_3d_plumes.py
+++ plot_3d_plumes.py
@@ -16,8 +16,8 @@ def cell_diffs(ds):
         cell_diffs[n+1] = cell_depths[n+1] - i
     return cell_diffs
 
-def zeta_and_out2(data_dir):
-    """Returns a dataarray of vorticity and a dataarray of buoyancy."""
+def calculate_output(data_dir,vertical_variable): 
+    """Returns a dataarray of vorticity."""
     # Regarding interpolating; don't forget to manually test this! (Both the interpolating and the vorticity.)
     # Uses the xgcm package for interpolating and various related calculations
     # https://xgcm.readthedocs.io/en/latest/xgcm-examples/02_mitgcm.html
@@ -25,47 +25,54 @@ def zeta_and_out2(data_dir):
     grid = xgcm.Grid(ds,periodic=False) 
     zeta = (-grid.diff(ds.U * ds.dxC, 'Y') + grid.diff(ds.V * ds.dyC, 'X'))/ds.rAz
     zeta = grid.interp(zeta,['X','Y'])
-    zeta = zeta.isel(XC=slice(0,51)).transpose('YC','XC','Z','time') # Slicing and interpolating for plotting reasons
-    ds['rho'] = ( ('time', 'Z', 'YC', 'XC'), density.linear(ds.S, ds.T, sref=35, tref=20, sbeta=0, talpha=0.0002, rhonil=1000)) #Constants from ./input/data          
+    zeta = zeta.isel(XC=slice(0,76)).transpose('YC','XC','Z','time') # Slicing and interpolating for plotting reasons
+    ds['rho'] = ( ('time', 'Z', 'YC', 'XC'), density.linear(ds.S, ds.T, sref=35, tref=20, sbeta=0, talpha=0.0002, rhonil=1000)) # SHOULD THIS BE T? PT? IS IT INSITU RHO???
+
     ds = ds.assign_coords(dz=('Zl',cell_diffs(ds)))
-    out2 = (-1) * (9.81) * (1/1000) * grid.diff(ds.rho, 'Z', boundary='extend')/ds['dz'] 
-    out2 = grid.interp(out2,'Z')
-    out2 = out2.isel(XC=slice(0,51)).transpose('YC','XC','Z','time')
-    return zeta, out2 
+    if vertical_variable=='N2': 
+        N2 = (-1) * (9.81) * (1/1000) * grid.diff(ds.rho, 'Z', boundary='extend')/ds['dz']  # replace this with the gsw function and CHECK IF IT SHOULD BE PD or INSITU D???
+        N2 = grid.interp(N2,'Z')
+        N2 = N2.isel(XC=slice(0,76)).transpose('YC','XC','Z','time')
+        out = N2
+    elif vertical_variable=='t': out = ds['T'].transpose('YC','XC','Z','time')
+    elif vertical_variable=='rho': out = ds['rho'].transpose('YC','XC','Z','time')
+    elif vertical_variable=='s': out = ds['S'].transpose('YC','XC','Z','time')
+    return zeta, out
 
-def get_mins_and_maxs(zeta,N2):
+def get_mins_and_maxs(da,plane):
     """Return the min and max values pertinent to the plots (i.e., zeta and N2)."""
-    zeta_min = zeta.isel(Z=0).min().to_numpy()
-    zeta_max = zeta.isel(Z=0).max().to_numpy()
-    zeta_minmax = zeta_min, zeta_max
-    N2_min = N2.isel(XC=-1,Z=slice(0,-1)).min().to_numpy()
-    N2_max = N2.isel(XC=-1,Z=slice(0,-1)).max().to_numpy()
-    N2_minmax = N2_min, N2_max
-    return zeta_minmax, N2_minmax
+    if plane=='horizontal':
+        min = da.isel(Z=0).min().to_numpy()
+        max = da.isel(Z=0).max().to_numpy()
+    elif plane=='vertical':
+        min = da.isel(XC=-1,Z=slice(0,-1)).min().to_numpy()
+        max = da.isel(XC=-1,Z=slice(0,-1)).max().to_numpy()
+    minmax = min, max
+    return minmax
 
-def plume_plot_engine(figs_dir, zeta, N2, i_time, zeta_minmax=None, N2_minmax=None, cb=False, dt=10):
+def plume_plot_engine(run, figs_dir, zeta, vertical_data, vertical_variable, i_time, zeta_minmax=None, vertical_minmax=None, cb=False, dt=10):
     """Plotting mechanism for creating ONE plot. 
     i_time is the index of the time you want to plot.
     minmax parameters are tuples.
     cb controls the colourbar; can either plot the cb or the fig, not both."""
     # https://matplotlib.org/stable/gallery/mplot3d/box3d.html#sphx-glr-gallery-mplot3d-box3d-py 
 
-    X, Y, Z = np.meshgrid(N2.XC.to_numpy(), N2.YC.to_numpy(), N2.Z.to_numpy())
+    X, Y, Z = np.meshgrid(zeta.XC.to_numpy(), zeta.YC.to_numpy(), zeta.Z.to_numpy())
 
     timestep_str = str(zeta.isel(time=i_time)['time'].dt.seconds.to_numpy()) #mightn't work > 1 day!!!!!!!
     time_hours_str = str(float(timestep_str)*dt/60/60) # Have to do this manually 
 
     zeta_np = zeta.isel(time=i_time).to_numpy()
-    N2_np = N2.isel(time=i_time).to_numpy()
+    vertical_np = vertical_data.isel(time=i_time).to_numpy()
 
     if zeta_minmax==None:
         zeta_min, zeta_max = zeta_np.min(), zeta_np.max()
     else:
         zeta_min, zeta_max = zeta_minmax
-    if N2_minmax==None:
-        N2_min, N2_max = N2_np.min(), N2_np.max()
+    if vertical_minmax==None:
+        vertical_min, vertical_max = vertical_np.min(), vertical_np.max()
     else:
-        N2_min, N2_max = N2_minmax
+        vertical_min, vertical_max = vertical_minmax
 
     linscale, linthresh = 0.001, 0.0001
     positive_levels = np.logspace(np.log10(linthresh), np.log10(zeta_max), 51)
@@ -78,16 +85,39 @@ def plume_plot_engine(figs_dir, zeta, N2, i_time, zeta_minmax=None, N2_minmax=No
         'norm': SymLogNorm(linscale=linscale, linthresh=linthresh, vmin=zeta_min, vmax=zeta_max),
     }
 
-    linscale, linthresh = 0.0001, 0.000002
-    positive_levels = np.logspace(np.log10(linthresh), np.log10(N2_max), 51)
-    negative_levels = -np.logspace(np.log10(linthresh), np.log10(-N2_min), 51)
-    levels = np.concatenate([negative_levels[::-1], [0], positive_levels])
-    kw_N2 = {
-        'vmin': N2_min,
-        'vmax': N2_max,
-        'levels': levels,
-        'norm': SymLogNorm(linscale=linscale, linthresh=linthresh, vmin=N2_min, vmax=N2_max),
-    }
+    if vertical_variable=='N2':
+        linscale, linthresh = 0.01, 0.0002 #0.0001, 0.000002
+        positive_levels = np.logspace(np.log10(linthresh), np.log10(vertical_max), 51)
+        negative_levels = -np.logspace(np.log10(linthresh), np.log10(-vertical_min), 51)
+        levels = np.concatenate([negative_levels[::-1], [0], positive_levels])
+        kw_vertical = {
+            'vmin': vertical_min,
+            'vmax': vertical_max,
+            'levels': levels,
+            'norm': SymLogNorm(linscale=linscale, linthresh=linthresh, vmin=vertical_min, vmax=vertical_max),
+        }
+        cm = 'RdGy'
+    elif vertical_variable=='rho':
+        kw_vertical = {
+            'levels': 102,
+            'vmin': vertical_min,
+            'vmax': vertical_max,
+            'norm': 'linear'}
+        cm = 'hot_r' #plt.colormaps['hot_r']
+    elif vertical_variable=='t':
+        kw_vertical = {
+            'levels': 102,
+            'vmin': vertical_min,
+            'vmax': vertical_max,
+            'norm': 'linear'}
+        cm = 'seismic' 
+    elif vertical_variable=='s':
+        kw_vertical = {
+            'levels': 102,
+            'vmin': vertical_min,
+            'vmax': vertical_max,
+            'norm': 'linear'}
+        cm = 'viridis' 
 
     # Create a figure with 3D ax
     fig = plt.figure(figsize=(5, 4))
@@ -99,9 +129,9 @@ def plume_plot_engine(figs_dir, zeta, N2, i_time, zeta_minmax=None, N2_minmax=No
         zdir='z', offset=0, cmap='seismic', **kw_zeta
     )
 
-    C_N2 = ax.contourf(
-        N2_np[:, -1, :-1], Y[:, -1, :-1], Z[:, -1, :-1], #the :-1's are to avoid plotting the bottom row of non-zero values
-        zdir='x', offset=X.max(), cmap='RdGy', **kw_N2
+    C_vertical = ax.contourf(
+        vertical_np[:, -1, :-1], Y[:, -1, :-1], Z[:, -1, :-1], #the :-1's are to avoid plotting the bottom row of non-zero values
+        zdir='x', offset=X.max(), cmap=cm, **kw_vertical
     )
 
     # Set limits of the plot from coord limits
@@ -129,7 +159,7 @@ def plume_plot_engine(figs_dir, zeta, N2, i_time, zeta_minmax=None, N2_minmax=No
 
     ax.xaxis._axinfo['juggled'] = (2,0,0)
 
-    ax.set_title('+'+time_hours_str + 'hrs')
+    ax.set_title(run+', +'+time_hours_str + 'hrs')
     
     if not os.path.exists(figs_dir):
         os.makedirs(figs_dir)
@@ -143,10 +173,11 @@ def plume_plot_engine(figs_dir, zeta, N2, i_time, zeta_minmax=None, N2_minmax=No
         cbar_zeta.formatter.set_powerlimits((0, 0))
         cbar_zeta.formatter.set_useMathText(True)
 
-        cbar_N2_ax = fig.add_axes([1.35,bbox_ax.y0, 0.025, bbox_ax.y1-bbox_ax.y0])
-        cbar_N2 = plt.colorbar(C_N2, cax=cbar_N2_ax, extend='both', shrink=0.5, pad=0.1, label='$N^2$ ($s^{-2}$)')
-        cbar_N2.formatter.set_powerlimits((0, 0))
-        cbar_N2.formatter.set_useMathText(True)
+        vertical_labels = {'N2': '$N^2$ ($s^{-2}$)', 'rho': r"$\rho$ ($kg$ $m^{-3}$)", 't': 'Temperature ($^\circ C$)', 's': 'Salinity ($PSU$)'}
+        cbar_vertical_ax = fig.add_axes([1.35,bbox_ax.y0, 0.025, bbox_ax.y1-bbox_ax.y0])
+        cbar_vertical = plt.colorbar(C_vertical, cax=cbar_vertical_ax, extend='both', shrink=0.5, pad=0.1, label=vertical_labels[vertical_variable])
+        cbar_vertical.formatter.set_powerlimits((0, 0))
+        cbar_vertical.formatter.set_useMathText(True)
 
         ax.remove()
         plt.savefig(figs_dir+'/plume_cbar_'+timestep_str+'.png',dpi=450,bbox_inches="tight")
@@ -157,19 +188,19 @@ def plume_plot_engine(figs_dir, zeta, N2, i_time, zeta_minmax=None, N2_minmax=No
         print(figs_dir+'/plume_'+timestep_str+'.png')
         plt.close()    
 
-def plot_plumes(figs_dir, zeta, N2, zeta_minmax, N2_minmax, i_time=None, dt=10):
+def plot_plumes(run, figs_dir, zeta, vertical_data, vertical_variable, zeta_minmax=None, vertical_minmax=None, i_time=None, dt=10):
     """Plot vorticity on the sea surface and buoyancy on the vertical face.
     Based on Vreugdenhil and Gayen 2021.
     Accepts datarrays of vorticity and buoyancy, as well as tuples of the min and max values to plot."""
 
     # Produces a colourbar
     i_time=10
-    plume_plot_engine(figs_dir, zeta, N2, i_time, zeta_minmax, N2_minmax, cb=True, dt=dt) #figs_dir, zeta, N2, i_time, zeta_minmax=None, N2_minmax=None, cb=False
+    plume_plot_engine(run, figs_dir, zeta, vertical_data, vertical_variable, i_time, zeta_minmax, vertical_minmax, cb=True, dt=dt) #figs_dir, zeta, N2, i_time, zeta_minmax=None, N2_minmax=None, cb=False
 
     # For making a movie
     i_times = len(zeta.time.to_numpy())
     for i_time in range(i_times):
-        plume_plot_engine(figs_dir, zeta, N2, i_time, zeta_minmax, N2_minmax, cb=False, dt=dt) #figs_dir, zeta, N2, i_time, zeta_minmax=None, N2_minmax=None, cb=False
+        plume_plot_engine(run, figs_dir, zeta, vertical_data, vertical_variable, i_time, zeta_minmax, vertical_minmax, cb=False, dt=dt) #figs_dir, zeta, N2, i_time, zeta_minmax=None, N2_minmax=None, cb=False
 
 def avg_temp(data_dir):
     """For comparing average temps across runs."""
@@ -200,55 +231,14 @@ if __name__ == "__main__":
     #avg_temp('../MITgcm/so_plumes/mrb_011') # 0.26413005811064794
     #quit()
 
-    run = 'mrb_002'
+    run = 'mrb_016'
+    vertical_variable = 'rho'
     data_dir = '/albedo/home/robrow001/MITgcm/so_plumes/'+run
-    figs_dir = '/albedo/home/robrow001/model_analyses/figs_'+run
-    zeta, out2 = zeta_and_out2(data_dir)
+    figs_dir = '/albedo/home/robrow001/model_analyses/figures/figs_'+run+'_'+vertical_variable
+    zeta, vertical_data = calculate_output(data_dir,vertical_variable)
+    #zeta_minmax = get_mins_and_maxs(zeta,'horizontal')
+    vertical_minmax = get_mins_and_maxs(vertical_data,'vertical')
+    print(vertical_minmax)
     zeta_minmax = (-0.04,0.04)#(-0.0075,0.0125)
-    out2_minmax = (-0.0002,0.0002)#(3,5)#(3.045,4.8)#(-0.00015,0.00015)#(-0.0000069,0.00000585)
-    plot_plumes(figs_dir, zeta, out2, zeta_minmax, out2_minmax, dt=3) #figs_dir, zeta, N2, zeta_minmax, N2_minmax, i_time=None
-    run = 'mrb_003'
-    data_dir = '/albedo/home/robrow001/MITgcm/so_plumes/'+run
-    figs_dir = '/albedo/home/robrow001/model_analyses/figs_'+run
-    zeta, out2 = zeta_and_out2(data_dir)
-    zeta_minmax = (-0.04,0.04)#(-0.0075,0.0125)
-    out2_minmax = (-0.0002,0.0002)#(3,5)#(3.045,4.8)#(-0.00015,0.00015)#(-0.0000069,0.00000585)
-    plot_plumes(figs_dir, zeta, out2, zeta_minmax, out2_minmax, dt=3) #figs_dir, zeta, N2, zeta_minmax, N2_minmax, i_time=None
-
-    run = 'mrb_004'
-    data_dir = '/albedo/home/robrow001/MITgcm/so_plumes/'+run
-    figs_dir = '/albedo/home/robrow001/model_analyses/figs_'+run
-    zeta, out2 = zeta_and_out2(data_dir)
-    zeta_minmax = (-0.04,0.04)#(-0.0075,0.0125)
-    out2_minmax = (-0.0002,0.0002)#(3,5)#(3.045,4.8)#(-0.00015,0.00015)#(-0.0000069,0.00000585)
-    plot_plumes(figs_dir, zeta, out2, zeta_minmax, out2_minmax, dt=3) #figs_dir, zeta, N2, zeta_minmax, N2_minmax, i_time=None 
-
-    run = 'mrb_005'
-    data_dir = '/albedo/home/robrow001/MITgcm/so_plumes/'+run
-    figs_dir = '/albedo/home/robrow001/model_analyses/figs_'+run
-    zeta, out2 = zeta_and_out2(data_dir)
-    zeta_minmax = (-0.04,0.04)#(-0.0075,0.0125)
-    out2_minmax = (-0.0002,0.0002)#(3,5)#(3.045,4.8)#(-0.00015,0.00015)#(-0.0000069,0.00000585)
-    plot_plumes(figs_dir, zeta, out2, zeta_minmax, out2_minmax, dt=3) #figs_dir, zeta, N2, zeta_minmax, N2_minmax, i_time=None
-    run = 'mrb_006'
-    data_dir = '/albedo/home/robrow001/MITgcm/so_plumes/'+run
-    figs_dir = '/albedo/home/robrow001/model_analyses/figs_'+run
-    zeta, out2 = zeta_and_out2(data_dir)
-    zeta_minmax = (-0.04,0.04)#(-0.0075,0.0125)
-    out2_minmax = (-0.0002,0.0002)#(3,5)#(3.045,4.8)#(-0.00015,0.00015)#(-0.0000069,0.00000585)
-    plot_plumes(figs_dir, zeta, out2, zeta_minmax, out2_minmax, dt=3) #figs_dir, zeta, N2, zeta_minmax, N2_minmax, i_time=None
-
-    run = 'mrb_007'
-    data_dir = '/albedo/home/robrow001/MITgcm/so_plumes/'+run
-    figs_dir = '/albedo/home/robrow001/model_analyses/figs_'+run
-    zeta, out2 = zeta_and_out2(data_dir)
-    zeta_minmax = (-0.04,0.04)#(-0.0075,0.0125)
-    out2_minmax = (-0.0002,0.0002)#(3,5)#(3.045,4.8)#(-0.00015,0.00015)#(-0.0000069,0.00000585)
-    plot_plumes(figs_dir, zeta, out2, zeta_minmax, out2_minmax, dt=3) #figs_dir, zeta, N2, zeta_minmax, N2_minmax, i_time=None
-    run = 'mrb_008'
-    data_dir = '/albedo/home/robrow001/MITgcm/so_plumes/'+run
-    figs_dir = '/albedo/home/robrow001/model_analyses/figs_'+run
-    zeta, out2 = zeta_and_out2(data_dir)
-    zeta_minmax = (-0.04,0.04)#(-0.0075,0.0125)
-    out2_minmax = (-0.0002,0.0002)#(3,5)#(3.045,4.8)#(-0.00015,0.00015)#(-0.0000069,0.00000585)
-    plot_plumes(figs_dir, zeta, out2, zeta_minmax, out2_minmax, dt=3) #figs_dir, zeta, N2, zeta_minmax, N2_minmax, i_time=None
+    vertical_minmax = (1003.8,1004.4)#(-0.0002,0.0002), (-2,1)
+    plot_plumes(run, figs_dir, zeta, vertical_data, vertical_variable, zeta_minmax=zeta_minmax, vertical_minmax=vertical_minmax, dt=3) #vertical_minmax 
